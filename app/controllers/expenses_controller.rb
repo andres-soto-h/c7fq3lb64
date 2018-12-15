49ef53b5e68c7cff5923419b96b26290fe3c04d8
Andrés Soto <andres.soto.h@outlook.com>
2018-12-15 13:39:39 -0500

    filter expenses by user

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,16 +1,18 @@
 class ExpensesController < ApplicationController
 
   before_action :authenticate_user!
-  
+
   def index
+    user = current_user
+
     if params[:concept].present? && !params[:category_id].present?
-      @expenses = Expense.where('concept LIKE (?)', "%#{params[:concept]}%")
+      @expenses = Expense.where('concept LIKE (?) AND user_id= ?', "%#{params[:concept]}%", user.id)
     elsif params[:concept].present? && params[:category_id].present?
-      @expenses = Expense.where('concept LIKE (?) AND category_id = ?', "%#{params[:concept]}%", params[:category_id].to_s)
+      @expenses = Expense.where('concept LIKE (?) AND category_id = ? AND user_id= ?', "%#{params[:concept]}%", params[:category_id].to_s, user.id)
     elsif !params[:concept].present? && params[:category_id].present?
       @expenses = Expense.where('category_id = ?', params[:category_id].to_s)
     else
-      @expenses = Expense.order('date DESC')
+      @expenses = Expense.where('user_id= ?', user.id)
      end
  end
 end
